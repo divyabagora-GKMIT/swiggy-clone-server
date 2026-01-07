178db5c6ba7c6ab078eca835e573eb18c20a04f2
divyabagora-GKMIT <divyabagora@gkmit.co>
2026-01-07 17:26:44 +0530

    feat: add rating entity

---
+++ src/ratings/entities/rating.entity.ts
@@ -0,0 +1,70 @@
+import { User } from 'src/users/entities/user.entity';
+import { Item } from 'src/items/entities/item.entity';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+import { Order } from 'src/orders/entites/order.entity';
+
+@Entity('ratings')
+export class Rating {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  /**
+   * User who gave the rating
+   */
+  @ManyToOne(() => User, (user) => user.ratings)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  /**
+   * Order related to this rating
+   */
+  //   @ManyToOne(() => Order, order => order.ratings)
+  //   @JoinColumn({ name: "order_id" })
+  //   order: Order;
+
+  /**
+   * Rated item (optional if rating is for restaurant)
+   */
+  @ManyToOne(() => Item, (item) => item.ratings, { nullable: true })
+  @JoinColumn({ name: 'item_id' })
+  item: Item;
+
+  /**
+   * Rated restaurant
+   */
+  @ManyToOne(() => Restaurant, (restaurant) => restaurant.ratings)
+  @JoinColumn({ name: 'restaurant_id' })
+  restaurant: Restaurant;
+
+  @Column({
+    type: 'float',
+    nullable: false,
+  })
+  rating: number;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: true,
+  })
+  comment: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: Date;
+}
