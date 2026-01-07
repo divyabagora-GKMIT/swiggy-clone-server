9df2de4c189aa6416f99e7a76e7f98c4cb381a8a
divyabagora-GKMIT <divyabagora@gkmit.co>
2026-01-07 15:35:05 +0530

    feat: add city entity

---
+++ src/cities/entities/city.entity.ts
@@ -0,0 +1,55 @@
+import { Exclude } from 'class-transformer';
+import { Address } from 'src/users/entities/address.entity';
+import { State } from 'src/states/entities/state.entity';
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  JoinColumn,
+  ManyToOne,
+} from 'typeorm';
+
+@Entity('cities')
+export class City {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    nullable: false,
+    unique: true,
+    length: 168,
+  })
+  name: string;
+
+  @ManyToOne(() => State, (state) => state.cities)
+  @JoinColumn({ name: 'state_id' })
+  state: State;
+
+  @ManyToOne(() => Address, (address) => address.city)
+  addresses: Address[];
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+
+  @Exclude()
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    nullable: true,
+    type: 'timestamp',
+  })
+  deletedAt: Date;
+}
